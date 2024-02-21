19de4527b510b120948be618eb06edf0aa7d7f68
fauzi45 <uzi.fajar45@gmail.com>
2024-02-21 16:55:24 +0700

    add location on migration user

--- server/migrations/20240220024216-create-user.js
+++ server/migrations/20240220024216-create-user.js
@@ -21,6 +21,9 @@ module.exports = {
       bio: {
         type: Sequelize.STRING
       },
+      location: {
+        type: Sequelize.STRING
+      },
       role: {
         type: Sequelize.INTEGER
       },
