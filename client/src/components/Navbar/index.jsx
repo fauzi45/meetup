3432acde7be14799931aee193bf35aee85fd6f28
fauzi45 <uzi.fajar45@gmail.com>
2024-02-22 15:07:53 +0700

    init crypto and react hot toast and change title navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -47,8 +47,7 @@ const Navbar = ({ title, locale, theme }) => {
     <div className={classes.headerWrapper} data-testid="navbar">
       <div className={classes.contentWrapper}>
         <div className={classes.logoImage} onClick={goHome}>
-          <img src="/vite.svg" alt="logo" className={classes.logo} />
-          <div className={classes.title}>{title}</div>
+          <div className={classes.title}>Meetup</div>
         </div>
         <div className={classes.toolbar}>
           <div className={classes.theme} onClick={handleTheme} data-testid="toggleTheme">
